949f8e612e6abdf5ce669d4b18a8c27f45cc4d6a
Estanislao <estanislao.sb@gmail.com>
2023-11-10 13:06:18 -0300

    working parallax - Needs fix
    the aparallax movement from Game is qorking ut needs to be fixed since the image is too big and makesit look weird

--- FlappyBird_Solution/FlappyBird/Game/Game.cpp
+++ FlappyBird_Solution/FlappyBird/Game/Game.cpp
@@ -15,9 +15,13 @@ static Image background_layer3;
 static Image background_layer4;
 
 static Texture2D background_layer1_text;
-static Texture2D background_layer2_text;
-static Texture2D background_layer3_text;
-static Texture2D background_layer4_text;
+static Texture2D background;
+static Texture2D midground;
+static Texture2D foreground;
+
+float scrollingBack = 0.0f;
+float scrollingMid = 0.0f;
+float scrollingFore = 0.0f;
 
 namespace Game
 {
@@ -35,23 +39,32 @@ namespace Game
 
         background_layer2 = LoadImage("../Resources/parallax_demon_woods_pack/layers/parallax-demon-woods-far-trees.png");
         ImageResize(&background_layer2, static_cast<int>(currentScreenConfig.width), static_cast<int>(currentScreenConfig.height));
-        background_layer2_text = LoadTextureFromImage(background_layer2);
+        background = LoadTextureFromImage(background_layer2);
         UnloadImage(background_layer2);
 
         background_layer3 = LoadImage("../Resources/parallax_demon_woods_pack/layers/parallax-demon-woods-mid-trees.png");
         ImageResize(&background_layer3, static_cast<int>(currentScreenConfig.width), static_cast<int>(currentScreenConfig.height));
-        background_layer3_text = LoadTextureFromImage(background_layer3);
+        midground = LoadTextureFromImage(background_layer3);
         UnloadImage(background_layer3);
 
         background_layer4 = LoadImage("../Resources/parallax_demon_woods_pack/layers/parallax-demon-woods-close-trees.png");
         ImageResize(&background_layer4, static_cast<int>(currentScreenConfig.width), static_cast<int>(currentScreenConfig.height));
-        background_layer4_text = LoadTextureFromImage(background_layer4);
+        foreground = LoadTextureFromImage(background_layer4);
         UnloadImage(background_layer4);
 
     }
 
     void UpdateGame()
     {
+        scrollingBack -= 0.1f;
+        scrollingMid -= 0.5f;
+        scrollingFore -= 1.0f;
+
+        // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
+        if (scrollingBack <= -background.width * 2) scrollingBack = 0;
+        if (scrollingMid <= -midground.width * 2) scrollingMid = 0;
+        if (scrollingFore <= -foreground.width * 2) scrollingFore = 0;
+
         DrawGame();
 
     }
@@ -62,11 +75,18 @@ namespace Game
 
         ClearBackground(RAYWHITE);
         DrawTexture(background_layer1_text, 0, 0, WHITE);
-        DrawTexture(background_layer2_text, 0, 0, WHITE);
-        DrawTexture(background_layer3_text, 0, 0, WHITE);
-        DrawTexture(background_layer4_text, 0, 0, WHITE);
+        DrawTextureEx(background, Vector2{ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+        DrawTextureEx(background, Vector2{ background.width * 2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
 
-        DrawText("FlappyBerd!", currentScreenConfig.width/3, 100, 40, LIGHTGRAY);
+        // Draw midground 
+        DrawTextureEx(midground, Vector2{ scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
+        DrawTextureEx(midground, Vector2{ midground.width * 2 + scrollingMid, 20 }, 0.0f, 2.0f, WHITE);
+
+        // Draw foreground
+        DrawTextureEx(foreground, Vector2{ scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
+        DrawTextureEx(foreground, Vector2{ foreground.width * 2 + scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
+
+        DrawText("FlappyBerd!", currentScreenConfig.width/3, 100, 30, LIGHTGRAY);
 
         EndDrawing();
     }
@@ -74,9 +94,9 @@ namespace Game
     void UnloadGame()
     {
         UnloadTexture(background_layer1_text);
-        UnloadTexture(background_layer2_text);
-        UnloadTexture(background_layer3_text);
-        UnloadTexture(background_layer4_text);
+        UnloadTexture(background);
+        UnloadTexture(midground);
+        UnloadTexture(foreground);
         std::cout << "Game Unloaded." << '\n';
     }
 }
